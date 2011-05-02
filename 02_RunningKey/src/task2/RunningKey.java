54351b5952a286f826cbd18980bb4c2754a28dcc
Henning Basold <h.basold@googlemail.com>
2011-05-02 14:28:11 +0200

    Schlüssel, Ver- und Entschlüsseln Running-Key

--- 02_RunningKey/src/task2/RunningKey.java
+++ 02_RunningKey/src/task2/RunningKey.java
@@ -13,9 +13,12 @@ package task2;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+import de.tubs.cs.iti.jcrypt.chiffre.CharacterMapping;
 import de.tubs.cs.iti.jcrypt.chiffre.Cipher;
 
 /**
@@ -25,6 +28,8 @@ import de.tubs.cs.iti.jcrypt.chiffre.Cipher;
  * @version 1.0 - Tue Mar 30 16:23:47 CEST 2010
  */
 public class RunningKey extends Cipher {
+  
+  private String keyFile;
 
   /**
    * Erzeugt einen neuen Schlüssel, der in der Klasse gespeichert wird,
@@ -33,7 +38,69 @@ public class RunningKey extends Cipher {
    * der Schlüssel gespeichert werden kann.
    */
   public void makeKey() {
-    System.out.println("Dummy für die Schlüsselerzeugung.");
+    BufferedReader standardInput = launcher.openStandardInput();
+    boolean accepted = false;
+    String msg = "Geeignete Werte für den Modulus werden in der Klasse "
+        + "'CharacterMapping'\nfestgelegt. Probieren Sie ggf. einen Modulus "
+        + "von 26, 27, 30 oder 31.\nDie Verschiebung muß größer oder gleich 0 "
+        + "und kleiner als der gewählte\nModulus sein.";
+    System.out.println(msg);
+    // Frage jeweils solange die Eingabe ab, bis diese akzeptiert werden kann.
+    do {
+      System.out.print("Geben Sie den Modulus ein: ");
+      try {
+        modulus = Integer.parseInt(standardInput.readLine());
+        if (modulus < 1) {
+          System.out.println("Ein Modulus < 1 wird nicht akzeptiert. Bitte "
+              + "korrigieren Sie Ihre Eingabe.");
+        } else {
+          // Prüfe, ob zum eingegebenen Modulus ein Default-Alphabet existiert.
+          String defaultAlphabet = CharacterMapping.getDefaultAlphabet(modulus);
+          if (!defaultAlphabet.equals("")) {
+            msg = "Vordefiniertes Alphabet: '" + defaultAlphabet
+                + "'\nDieses vordefinierte Alphabet kann durch Angabe einer "
+                + "geeigneten Alphabet-Datei\nersetzt werden. Weitere "
+                + "Informationen finden Sie im Javadoc der Klasse\n'Character"
+                + "Mapping'.";
+            System.out.println(msg);
+            accepted = true;
+          } else {
+            msg = "Warnung: Dem eingegebenen Modulus kann kein Default-"
+                + "Alphabet zugeordnet werden.\nErstellen Sie zusätzlich zu "
+                + "dieser Schlüssel- eine passende Alphabet-Datei.\nWeitere "
+                + "Informationen finden Sie im Javadoc der Klasse 'Character"
+                + "Mapping'.";
+            System.out.println(msg);
+            accepted = true;
+          }
+        }
+      } catch (NumberFormatException e) {
+        System.out.println("Fehler beim Parsen des Modulus. Bitte korrigieren"
+            + " Sie Ihre Eingabe.");
+      } catch (IOException e) {
+        System.err
+            .println("Abbruch: Fehler beim Lesen von der Standardeingabe.");
+        e.printStackTrace();
+        System.exit(1);
+      }
+    } while (!accepted);
+    accepted = false;
+    do {
+      try {
+        System.out.print("Geben Sie den Pfad zur Datei mit dem Schlüsseltext ein: ");
+        keyFile = standardInput.readLine();
+        if ((new File(keyFile)).exists()) {
+          accepted = true;
+        } else {
+          System.out.println("Datei existiert nicht!");
+        }
+      } catch (IOException e) {
+        System.err
+            .println("Abbruch: Fehler beim Lesen von der Standardeingabe.");
+        e.printStackTrace();
+        System.exit(1);
+      }
+    } while (!accepted);
   }
 
   /**
@@ -55,10 +122,8 @@ public class RunningKey extends Cipher {
    */
   public void writeKey(BufferedWriter key) {
     try {
-//      key.write(modulus + " " + shifts.length);
-//      for (int i = 0; i < shifts.length; i++) {
-//        key.write(" " + shifts[i]);
-//      }
+      key.write(modulus + " ");
+      key.write(keyFile);
       key.newLine();
       key.close();
     } catch (IOException e) {
@@ -80,16 +145,12 @@ public class RunningKey extends Cipher {
    */
   public void readKey(BufferedReader key) {
     try {
-      StringTokenizer st = new StringTokenizer(key.readLine(), " ");
-//      modulus = Integer.parseInt(st.nextToken());
-//      System.out.println("Modulus: " + modulus);
-//      int numberOfShifts = Integer.parseInt(st.nextToken());
-//      System.out.println(numberOfShifts + " Verschiebungen: ");
-//      shifts = new int[numberOfShifts];
-//      for (int i = 0; i < numberOfShifts; i++) {
-//        shifts[i] = Integer.parseInt(st.nextToken());
-//        System.out.println(" " + shifts[i]);
-//      }
+      String line = key.readLine();
+      int modulEnd = line.indexOf(' ');
+      modulus = Integer.parseInt(line.substring(0, modulEnd));
+      System.out.println("Modulus: " + modulus);
+      keyFile = line.substring(modulEnd + 1);
+      System.out.println("Key file: " + keyFile);
       key.close();
     } catch (IOException e) {
       System.err.println("Abbruch: Fehler beim Lesen oder Schließen der "
@@ -119,6 +180,63 @@ public class RunningKey extends Cipher {
    */
   public void encipher(BufferedReader cleartext, BufferedWriter ciphertext) {
 
+    // An dieser Stelle könnte man alle Zeichen, die aus der Klartextdatei
+    // gelesen werden, in Klein- bzw. Großbuchstaben umwandeln lassen:
+    // charMap.setConvertToLowerCase();
+    // charMap.setConvertToUpperCase();
+
+    try {
+      FileReader key = new FileReader(keyFile);
+      int shift = -1;
+      // 'character' ist die Integer-Repräsentation eines Zeichens.
+      int character;
+      // 'characterSkipped' zeigt an, daß ein aus der Klartextdatei gelesenes
+      // Zeichen mit dem gewählten Alphabet nicht abgebildet werden konnte.
+      boolean characterSkipped = false;
+      // Lese zeichenweise aus der Klartextdatei, bis das Dateiende erreicht
+      // ist. Der Buchstabe a wird z.B. als ein Wert von 97 gelesen.
+      while ((character = cleartext.read()) != -1
+          && (shift = key.read()) != -1) {
+        // Bilde 'character' auf dessen interne Darstellung ab, d.h. auf einen
+        // Wert der Menge {0, 1, ..., Modulus - 1}. Ist z.B. a der erste
+        // Buchstabe des Alphabets, wird die gelesene 97 auf 0 abgebildet:
+        // mapChar(97) = 0.
+        character = charMap.mapChar(character);
+        shift = charMap.mapChar(shift);
+        if (character != -1 && shift != -1) {
+          // Das gelesene Zeichen ist im benutzten Alphabet enthalten und konnte
+          // abgebildet werden. Die folgende Quellcode-Zeile stellt den Kern der
+          // Caesar-Chiffrierung dar: Addiere zu (der internen Darstellung von)
+          // 'character' zyklisch den 'shift' hinzu.
+          character = (character + shift) % modulus;
+          // Das nun chiffrierte Zeichen wird von der internen Darstellung in
+          // die Dateikodierung konvertiert. Ist z.B. 1 das Ergebnis der
+          // Verschlüsselung (also die interne Darstellung für b), so wird dies
+          // konvertiert zu 98: remapChar(1) = 98. Der Wert 98 wird schließlich
+          // in die Chiffretextdatei geschrieben.
+          character = charMap.remapChar(character);
+          ciphertext.write(character);
+        } else {
+          // Das gelesene Zeichen ist im benutzten Alphabet nicht enthalten.
+          characterSkipped = true;
+        }
+      }
+      if (characterSkipped) {
+        System.out.println("Warnung: Mindestens ein Zeichen aus der "
+            + "Klartextdatei oder dem Schlüsseltext ist im Alphabet nicht\nenthalten und wurde "
+            + "überlesen.");
+      }
+      else if(character != -1 && shift == -1){
+        System.out.println("Schlüsseltext zu kurz!");
+      }
+      cleartext.close();
+      ciphertext.close();
+    } catch (IOException e) {
+      System.err.println("Abbruch: Fehler beim Zugriff auf Klar- oder "
+          + "Chiffretextdatei.");
+      e.printStackTrace();
+      System.exit(1);
+    }
   }
 
   /**
@@ -135,6 +253,32 @@ public class RunningKey extends Cipher {
    */
   public void decipher(BufferedReader ciphertext, BufferedWriter cleartext) {
 
+    // Kommentierung analog 'encipher(cleartext, ciphertext)'.
+    try {
+      FileReader key = new FileReader(keyFile);
+      int shift = -1;
+      int character;
+      while ((character = ciphertext.read()) != -1
+          && (shift = key.read()) != -1) {
+        character = charMap.mapChar(character);
+        shift = charMap.mapChar(shift);
+        if (character != -1 && shift != -1) {
+          character = (character - shift + modulus) % modulus;
+          character = charMap.remapChar(character);
+          cleartext.write(character);
+        } else {
+          // Ein überlesenes Zeichen sollte bei korrekter Chiffretext-Datei
+          // eigentlich nicht auftreten können.
+        }
+      }
+      cleartext.close();
+      ciphertext.close();
+    } catch (IOException e) {
+      System.err.println("Abbruch: Fehler beim Zugriff auf Klar- oder "
+          + "Chiffretextdatei.");
+      e.printStackTrace();
+      System.exit(1);
+    }
   }
 
   /**
