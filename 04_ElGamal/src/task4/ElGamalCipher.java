1716f9c968b19c4cc5cd2c95d47d75ed19c778b5
Henning Basold <h.basold@googlemail.com>
2011-05-23 14:40:39 +0200

    ElGamal-Schlüsselerzeugung

--- 04_ElGamal/src/task4/ElGamalCipher.java
+++ 04_ElGamal/src/task4/ElGamalCipher.java
@@ -15,7 +15,13 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Random;
 
+import de.tubs.cs.iti.jcrypt.chiffre.BigIntegerUtil;
 import de.tubs.cs.iti.jcrypt.chiffre.BlockCipher;
 
 /**
@@ -25,7 +31,15 @@ import de.tubs.cs.iti.jcrypt.chiffre.BlockCipher;
  * @version 1.1 - Sat Apr 03 22:06:35 CEST 2010
  */
 public final class ElGamalCipher extends BlockCipher {
+  
+  private BigInteger p;
+  private BigInteger g;
+  private BigInteger x;
+  private BigInteger y;
 
+  private String privateKeyFile;
+  private String publicKeyFile;
+  
   /**
    * Entschlüsselt den durch den FileInputStream <code>ciphertext</code>
    * gegebenen Chiffretext und schreibt den Klartext in den FileOutputStream
@@ -67,8 +81,73 @@ public final class ElGamalCipher extends BlockCipher {
    * @see #writeKey writeKey
    */
   public void makeKey() {
+    BufferedReader standardInput = launcher.openStandardInput();
+    boolean accepted = false;
+    
+    int bitLength = 0;
+    
+    // Frage jeweils solange die Eingabe ab, bis diese akzeptiert werden kann.
+    do {
+      System.out.print("Geben Sie den Pfad zu der Schlüsseldatei für den privaten Schlüssel ein: ");
+      try {
+        privateKeyFile = standardInput.readLine();
+        if (privateKeyFile.isEmpty()) {
+          System.out.println("Dateinamen eingeben!");
+        } else {
+          publicKeyFile = privateKeyFile + ".public";
+          accepted = true;
+        }
+      } catch (NumberFormatException e) {
+        // kann nicht auftreten
+      } catch (IOException e) {
+        System.err
+            .println("Abbruch: Fehler beim Lesen von der Standardeingabe.");
+        e.printStackTrace();
+        System.exit(1);
+      }
+    } while (!accepted);
+    
+    accepted = false;
+   
+    do {
+      System.out.print("Geben Sie die gewünschte Bitlänge ein (min. 512, max. 2048 Bit): ");
+      try {
+        bitLength = Integer.parseInt(standardInput.readLine());
+        if (bitLength < 512 || bitLength > 2048) {
+          System.out.println("Bitlänge zu groß/klein.");
+        } else {
+          accepted = true;
+        }
+      } catch (NumberFormatException e) {
+        System.out.println("Bitlänge ungültig.");
+      } catch (IOException e) {
+        System.err
+            .println("Abbruch: Fehler beim Lesen von der Standardeingabe.");
+        e.printStackTrace();
+        System.exit(1);
+      }
+    } while (!accepted);
+    
 
-    System.out.println("Dummy für die Schlüsselerzeugung.");
+    Random rnd = new Random(System.currentTimeMillis());
+    BigInteger q, p;
+    do {
+      q = BigInteger.probablePrime(bitLength, rnd);
+      p = q.multiply(BigInteger.valueOf(2)).add(BigInteger.ONE);
+    } while(!p.isProbablePrime(100));
+    
+    BigInteger g;
+    BigInteger minus1ModP = p.subtract(BigInteger.ONE); // BigInteger.valueOf(-1).mod(p);
+    do {
+      g = BigIntegerUtil.randomBetween(BigInteger.valueOf(2), p.subtract(BigInteger.ONE), rnd);
+    } while(!g.modPow(q, p).equals(minus1ModP));
+    
+    System.out.println("q=" + q + ", p= " + p + ", g=" + g);
+    
+    this.p = p;
+    this.g = g;
+    x = BigIntegerUtil.randomBetween(BigInteger.ONE, p.subtract(BigInteger.ONE), rnd);
+    y = g.modPow(x, p);
   }
 
   /**
@@ -80,7 +159,27 @@ public final class ElGamalCipher extends BlockCipher {
    * @see #writeKey writeKey
    */
   public void readKey(BufferedReader key) {
-
+    try {
+      privateKeyFile = key.readLine();
+      publicKeyFile = key.readLine();
+      
+      BufferedReader in = new BufferedReader(new FileReader(privateKeyFile));
+      p = new BigInteger(in.readLine(), 16);
+      g = new BigInteger(in.readLine(), 16);
+      x = new BigInteger(in.readLine(), 16);
+      
+      in = new BufferedReader(new FileReader(publicKeyFile));
+      BigInteger p_ = new BigInteger(in.readLine(), 16);
+      BigInteger g_ = new BigInteger(in.readLine(), 16);
+      y = new BigInteger(in.readLine(), 16);
+      
+      assert p.equals(p_);
+      assert g.equals(g_);
+      
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
   /**
@@ -92,6 +191,25 @@ public final class ElGamalCipher extends BlockCipher {
    * @see #readKey readKey
    */
   public void writeKey(BufferedWriter key) {
-
+    try {
+      key.write(privateKeyFile); key.newLine();
+      key.write(publicKeyFile); key.newLine();
+      
+      BufferedWriter out = new BufferedWriter(new FileWriter(privateKeyFile));
+      out.write(p.toString(16)); out.newLine();
+      out.write(g.toString(16)); out.newLine();
+      out.write(x.toString(16)); out.newLine();
+      out.close();      
+      
+      out = new BufferedWriter(new FileWriter(publicKeyFile));
+      out.write(p.toString(16)); out.newLine();
+      out.write(g.toString(16)); out.newLine();
+      out.write(y.toString(16)); out.newLine();
+      out.close();
+      
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
