6a8d3e7d307f28fa54c09a2dc02a991a1f8b12af
Henning Basold <h.basold@googlemail.com>
2011-06-06 14:47:16 +0200

    Projekt für Teil 2 (Protokolle) angelegt und Station-To-Station eingerichtet

---
+++ protokoll/src/de/tubs/cs/iti/krypto/protokoll/station2station/S2SProtocol.java
@@ -0,0 +1,44 @@
+package de.tubs.cs.iti.krypto.protokoll.station2station;
+
+import de.tubs.cs.iti.krypto.protokoll.Communicator;
+import de.tubs.cs.iti.krypto.protokoll.Protocol;
+
+public class S2SProtocol implements Protocol {
+
+  @Override
+  public void setCommunicator(Communicator Com) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public String nameOfTheGame() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void sendFirst() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void receiveFirst() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public int minPlayer() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int maxPlayer() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+}
